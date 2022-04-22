a34a0dd60f67824506f33907d12bbf8b0aaa6897
Hernán Vargas <hern.var@gmail.com>
2022-04-22 01:56:39 -0400

    Basic hypothesis view

--- src/pages/hypotheses.tsx
+++ src/pages/hypotheses.tsx
@@ -1,12 +1,14 @@
+import { Box, Card, InputAdornment, MenuItem, Select, SelectChangeEvent, TextField, Typography } from "@mui/material";
 import { DISKAPI } from "DISK/API";
 import { Hypothesis } from "DISK/interfaces";
 import React, { useEffect } from "react";
+import SearchIcon from '@mui/icons-material/Search';
+
+type OrderType = 'date'|'author';
 
 export const Hypotheses = () => {
-    //const hypotheses : Hypothesis[] = await DISKAPI.getHypotheses();
-
     const [hypotheses, setHypotheses] = React.useState<Hypothesis[]>([]);
-    //componentDidMount(() => { console.log("asdqwe"); })
+    const [order, setOrder] = React.useState<OrderType>('date');
 
     useEffect(() => {
         let hypP : Promise<Hypothesis[]> =DISKAPI.getHypotheses();
@@ -15,10 +17,29 @@ export const Hypotheses = () => {
         })
     }, []);
 
+    const handleChangeOrder = (event: SelectChangeEvent<OrderType>) => {
+        let order : OrderType = event.target!.value as OrderType;
+        if (order) setOrder(order);
+    }
+
     return (
-        <div>
-            <span>Hypotheses</span>
-            {hypotheses.map((h:Hypothesis) => <div key={h.id}> {h.name} </div>)}
-        </div>
+        <Box>
+            <Box sx={{display:'flex', paddingBottom: "5px"}}>
+                <TextField id="input-text-search" label="Search hypotheses" variant="outlined" size="small" 
+                    sx={{width:'100%', paddingRight:'5px'}} InputProps={{
+                    startAdornment: <InputAdornment position="start"> <SearchIcon/> </InputAdornment>
+                }}/>
+                <Select id="select-order" value={order} label="Order" onChange={handleChangeOrder} size="small">
+                    <MenuItem value={'date'}>Date</MenuItem>
+                    <MenuItem value={'author'}>Author</MenuItem>
+                </Select>
+            </Box>
+            <Card variant="outlined" sx={{height: "calc(100vh - 157px)"}}>
+                {hypotheses.map((h:Hypothesis) => <Box key={h.id}>
+                    <Typography variant="h6"> {h.name} </Typography>
+                    <Typography> {h.description} </Typography>
+                </Box>)}
+            </Card>
+        </Box>
     )
 }
