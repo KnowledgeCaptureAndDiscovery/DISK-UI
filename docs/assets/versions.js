fc73fbb860af676861ce8233f91f084a8482ae5b
Maximilinano Osorio <mosorio@isi.edu>
2022-10-24 15:56:48 -0300

    fix: disable version selector

--- docs/assets/versions.js
+++ docs/assets/versions.js
@@ -1,24 +1,9 @@
 setTimeout(function() {
   const callbackName = 'callback_' + new Date().getTime();
   window[callbackName] = function (response) {
-  const div = document.createElement('div');
-  div.innerHTML = response.html;
-  document.querySelector(".md-header__inner > .md-header__title").appendChild(div);
-  const container = div.querySelector('.rst-versions');
   var caret = document.createElement('div');
   caret.innerHTML = "<i class='fa fa-caret-down dropdown-caret'></i>"
   caret.classList.add('dropdown-caret')
-  div.querySelector('.rst-current-version').appendChild(caret);
-  div.querySelector('.rst-current-version').addEventListener('click', function() {
-      const classes = container.className.split(' ');
-      const index = classes.indexOf('shift-up');
-      if (index === -1) {
-          classes.push('shift-up');
-      } else {
-          classes.splice(index, 1);
-      }
-      container.className = classes.join(' ');
-  });
   }
 
   var CSSLink = document.createElement('link');
